ff6776fc9cf4be4b97398978b0d33110f8599eaa
zcdev <zcdev@hotmail.com>
2025-10-19 19:41:09 -0700

    feat(type): Implement and refactor Global types for new items to match data - Add props to Avatar - Add ForceRelation and Forces - Update PalaxyState

--- src/app/palaxy/types.ts
+++ src/app/palaxy/types.ts
@@ -4,16 +4,33 @@ export type ViewPhase = 'select' | 'intro' | 'forces' | 'pulse' | 'thanks';
 
 export interface PalaxyState {
     phase: ViewPhase;
+    avatar?: Avatar;
     selectedAvatar?: Avatar;
     surveyResponses: Record<string, string>;
+}
+
+export interface ForceRelation {
+    id: string;
+    note: string;
+}
+
+export interface Forces {
+    neighbors: ForceRelation[];
+    counterforce: ForceRelation;
 }
 
 export interface Avatar {
     id: string;
     name: string;
-    tagline: string;
+    title: string;
     intro: string;
+    bio: string;
     image: string;
-    forces: string[];
-    counterforce: string;
+    forces: Forces;
+    species: string;
+    homeworld: string;
+    planet: string;
+    synergy: string;
+    psyche: string;
+    wavelength: string;
 }
