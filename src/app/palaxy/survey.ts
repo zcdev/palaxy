80ec6a4ca539822add56ce66707a9cd1085aa504
zcdev <zcdev@hotmail.com>
2025-10-20 21:43:51 -0700

    feat(survey): add survey question data: id, text options

---
+++ src/app/palaxy/survey.ts
@@ -0,0 +1,27 @@
+export const questions = [
+    {
+        "id": "q1",
+        "text": "So… Did the galaxy’s personality algorithm totally nail you?",
+        "options": ["Very accurate", "Close enough", "Who did this?"],
+    },
+    {
+        "id": "q2",
+        "text": "Who was your Palaxy cosmic identity?",
+        "options": ["Flarb", "Glimbo", "Blip", "Orbitz", "Icy-7", "Zarnok"],
+    },
+    {
+        "id": "q3",
+        "text": "Would you trust your cosmic match to pilot your spaceship?",
+        "options": ["Absolutely", "Maybe on autopilot", "Only if seatbelts exist"],
+    },
+    {
+        "id": "q4",
+        "text": "How emotionally scarred are you after reading your bio?",
+        "options": ["Enlightened", "Amused", "Filing a complaint with Palaxy HR"],
+    },
+    {
+        "id": "q5",
+        "text": "Would you recommend Palaxy to your species?",
+        "options": ["Yes", "Depends on snack availability", "Only under duress"],
+    },
+] as const;
