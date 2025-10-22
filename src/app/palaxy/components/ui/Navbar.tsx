282497b3abf42214f318fe3a3b0b1e4a30434f39
zcdev <zcdev@hotmail.com>
2025-10-22 11:32:04 -0700

    style(mobile): restyle mobile Navbar

--- src/app/palaxy/components/ui/Navbar.tsx
+++ src/app/palaxy/components/ui/Navbar.tsx
@@ -1,9 +1,9 @@
 import { LuOrbit } from "react-icons/lu";
 export default function Navbar() {
     return (
-        <nav className="fixed top-[4%] left-[5.5%] w-full z-50">
-            <a href="/" className="text-[40px] text-white/70 hover:text-purple-400 transition flex items-center content-center">
-                <LuOrbit strokeWidth={2.5} className="inline mr-2" /><span className="font-headline text-[26px] pt-2">Palaxy</span>
+        <nav className="navbar fixed top-0 md:top-[4%] left-[2.2%] md:left-[5.5%] w-full z-50 py-2">
+            <a href="/" aria-label="Palaxy home" className="text-[40px] text-white/70 hover:text-purple-400 transition flex items-center content-center">
+                <LuOrbit strokeWidth={2.5} aria-hidden="true" className="inline mr-2" /><span aria-hidden="true" className="font-headline text-[26px] pt-2 hidden sm:inline">Palaxy</span>
             </a>
         </nav>
     );
