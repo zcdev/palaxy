cd1983790248d999ed9c5d919357b5252e7becea
zcdev <zcdev@hotmail.com>
2026-03-07 13:45:52 -0800

    style(background): adjust purple sky star container height for mobile viewport background coverage

--- src/app/components/ui/Background.tsx
+++ src/app/components/ui/Background.tsx
@@ -4,7 +4,7 @@ import Stars from '../../components/background/Stars';
 
 export default function Background() {
     return (
-        <div className="fixed inset-0 -z-10 bg-gradient-to-b from-indigo-950 via-purple-900 to-black">
+        <div className="fixed h-screen inset-0 -z-10 bg-gradient-to-b from-indigo-950 via-purple-900 to-black">
             <Stars />
         </div>
     );
